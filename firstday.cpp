27d4ca2666b1f89c2615105533b114ca73330a95
Shree Charan N <163136607+Charan291005@users.noreply.github.com>
2026-06-01 22:46:59 +0530

    day 1

---
+++ firstday.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    cout << "Myself Charan \nmy hobby is to learn cpp";  //details
+    return 0;
+}
+
+int main() {
+    int age = 25;
+    cout << age << "\n";
+    return 0;
+}
