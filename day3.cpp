1e04bd85049047243a8c3755df20a94c0c4ad5e4
Shree Charan N <163136607+Charan291005@users.noreply.github.com>
2026-06-04 01:00:59 +0530

    Add files via upload

---
+++ day3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n =5;
+
+    for ( int i =0; i <= n-1; i++) {
+
+        for (int j =0; j <= n-1; j++) {
+            cout << j << "";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
