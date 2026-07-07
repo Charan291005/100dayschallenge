8e7ae404e3b8ef03024f9e0624f6e411ed21f092
Shree Charan N <163136607+Charan291005@users.noreply.github.com>
2026-07-07 23:10:34 +0530

    fibonnaci using recursion

---
+++ Day 22/fibonnaci.py
@@ -0,0 +1,11 @@
+n = int(input())
+class solution:
+
+    def fib(self,n:int)->int:
+        if n==0 or n==1:
+            return n
+
+        return self.fib(n-1)+self.fib(n-2)
+
+solu = solution()
+print(solu.fib(n))
